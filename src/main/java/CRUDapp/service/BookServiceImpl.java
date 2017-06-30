7d154fd32267a8149d21186fb1fcda6ef1b50c38
Alexander <berezkin05@gmail.com>
2017-07-01 00:14:54 +0300

    last changes

--- src/main/java/CRUDapp/service/BookServiceImpl.java
+++ src/main/java/CRUDapp/service/BookServiceImpl.java
@@ -1,7 +1,47 @@
 package CRUDapp.service;
 
+import CRUDapp.dao.BookDao;
+import CRUDapp.model.Book;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 /**
  * Created by Alexander on 01/07/2017.
  */
-public class BookServiceImpl {
+@Service
+public class BookServiceImpl implements BookService {
+
+    private BookDao bookDao;
+
+    @Override
+    @Transactional
+    public void addBook(Book book) {
+        this.bookDao.addBook(book);
+    }
+
+    @Override
+    @Transactional
+    public void updateBook(Book book) {
+        bookDao.updateBook(book);
+    }
+
+    @Override
+    @Transactional
+    public void deleteBook(long id) {
+        bookDao.deleteBook(id);
+    }
+
+    @Override
+    @Transactional
+    public Book getBookById(long id) {
+        return bookDao.getBookById(id);
+    }
+
+    @Override
+    @Transactional
+    public List<Book> listBooks() {
+        return bookDao.listBooks();
+    }
 }
